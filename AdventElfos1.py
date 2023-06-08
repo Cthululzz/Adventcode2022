b51ab61b6ee1fd92af849220aaf3181c7e3f9d11
Cthululzz <daniel.silva.ruz@gmail.com>
2023-06-08 12:38:39 -0400

    init commit

---
+++ AdventElfos1.py
@@ -0,0 +1,14 @@
+#Encontrar al elfo con la mayor cantidad de calorias 
+#Son 5 elfos, llevan calorias de formas separadas
+
+lista = [
+    [1000, 2000, 3000],
+    [4000],
+    [5000, 6000],
+    [7000, 8000, 9000],
+    [10000]
+]
+
+print(lista)
+
+
